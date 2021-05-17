d694b72f67276dc2aa43b40e1efa4adf2640b84a
unknown <com>
2021-05-17 01:14:01 -0400

    Added cities

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,12 +14,27 @@ const Navbar = (props) => {
             <p className="hide-btn" onClick={handleClick} >Open/Close</p>
             <div className="nav-content" style={{display: hideShowElement }}>
                 <ul>
-                    <li onClick={() => { props.handleClickHome('pZcKpivKu5Y') }}>Toronto</li>
-                    <li onClick={() => { props.handleClickHome('h-tk7l-zVjg') }}>Hong Kong</li>
-                    <li onClick={() => { props.handleClickHome('aK8o2R_FDmI')}}>Ikebukuro</li>
-                    <li onClick={() => { props.handleClickHome('sdzh5HT2bmQ')}}>Seoul</li>
-                    <li onClick={() => { props.handleClickHome('Y9kJjagrOdc')}}>Shibuya</li>
-                    <li onClick={() => { props.handleClickHome('w_NM14iljsE')}}>City Bus: Seoul</li>
+                    <li className="nav-button" onClick={() => { 
+                        props.handleClickHome('pZcKpivKu5Y') }}>Toronto</li>
+                    <li className="nav-button" onClick={() => { 
+                        props.handleClickHome('h-tk7l-zVjg') }}>Hong Kong</li>
+                    <li className="nav-button" onClick={() => { 
+                        props.handleClickHome('aK8o2R_FDmI')}}>Ikebukuro</li>
+                    <li className="nav-button" onClick={() => { 
+                        props.handleClickHome('xnybGgcArSU')}}>Macau </li>
+                    <li className="nav-button" onClick={() => { 
+                        props.handleClickHome('sdzh5HT2bmQ')}}>Seoul</li>
+                    <li className="nav-button" onClick={() => { 
+                        props.handleClickHome('Y9kJjagrOdc')}}>Shibuya</li>
+                    <li className="nav-button" onClick={() => { 
+                        props.handleClickHome('HHzkIWaKC54')}}>Taipei</li>
+                    <li className="nav-button" onClick={() => { 
+                        props.handleClickHome('w_NM14iljsE')}}>City Bus: Seoul</li>
+                </ul> 
+                <ul>
+                    <li className="nav-button">
+                        <a href='https://github.com/StudyCamp/arriveandlisten' 
+                            target="_blank">Github</a></li>
                 </ul>
             </div>
        
